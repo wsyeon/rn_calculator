06087a4d998f198f22c475e6de56bebcfd43af1c
C팀 <sywon2005@gmail.com>
2023-08-08 14:33:25 +0900

    eval()로 계산 기능

--- components/Inputs.js
+++ components/Inputs.js
@@ -2,36 +2,51 @@ import React from 'react';
 import {View, StyleSheet, Platform} from 'react-native';
 import InputsBtn from './InputsBtn';
 
-const Inputs = () => {
+const Inputs = ({setInputs}) => {
+  const onClickBtn = name => {
+    if (name === 'C') {
+      setInputs('');
+      return false;
+    }
+    if (name === '=') {
+      setInputs(pre => eval(pre));
+      return false;
+    }
+    if (name === 'X') {
+      setInputs(pre => pre + '*');
+    } else {
+      setInputs(pre => pre + name);
+    }
+  };
   return (
     <View style={styles.block}>
       <View style={styles.btnGroup}>
-        <InputsBtn btnName="C" />
-        <InputsBtn btnName="7" />
-        <InputsBtn btnName="4" />
-        <InputsBtn btnName="1" />
-        <InputsBtn btnName="+/-" />
+        <InputsBtn onClickBtn={onClickBtn} redBtnColor btnName="C" />
+        <InputsBtn onClickBtn={onClickBtn} btnName="7" />
+        <InputsBtn onClickBtn={onClickBtn} btnName="4" />
+        <InputsBtn onClickBtn={onClickBtn} btnName="1" />
+        <InputsBtn onClickBtn={onClickBtn} disableBtn btnName="+/-" />
       </View>
       <View style={styles.btnGroup}>
-        <InputsBtn btnName="()" />
-        <InputsBtn btnName="8" />
-        <InputsBtn btnName="5" />
-        <InputsBtn btnName="2" />
-        <InputsBtn btnName="0" />
+        <InputsBtn greenBtnColor disableBtn btnName="()" />
+        <InputsBtn onClickBtn={onClickBtn} btnName="8" />
+        <InputsBtn onClickBtn={onClickBtn} btnName="5" />
+        <InputsBtn onClickBtn={onClickBtn} btnName="2" />
+        <InputsBtn onClickBtn={onClickBtn} btnName="0" />
       </View>
       <View style={styles.btnGroup}>
-        <InputsBtn btnName="%" />
-        <InputsBtn btnName="9" />
-        <InputsBtn btnName="6" />
-        <InputsBtn btnName="3" />
-        <InputsBtn btnName="." />
+        <InputsBtn greenBtnColor disableBtn btnName="÷" />
+        <InputsBtn onClickBtn={onClickBtn} btnName="9" />
+        <InputsBtn onClickBtn={onClickBtn} btnName="6" />
+        <InputsBtn onClickBtn={onClickBtn} btnName="3" />
+        <InputsBtn onClickBtn={onClickBtn} btnName="." />
       </View>
       <View style={styles.btnGroup}>
-        <InputsBtn btnName="/" />
-        <InputsBtn btnName="X" />
-        <InputsBtn btnName="-" />
-        <InputsBtn btnName="+" />
-        <InputsBtn btnName="=" />
+        <InputsBtn onClickBtn={onClickBtn} greenBtnColor btnName="/" />
+        <InputsBtn onClickBtn={onClickBtn} greenBtnColor btnName="X" />
+        <InputsBtn onClickBtn={onClickBtn} greenBtnColor btnName="-" />
+        <InputsBtn onClickBtn={onClickBtn} greenBtnColor btnName="+" />
+        <InputsBtn greenBtn onClickBtn={onClickBtn} btnName="=" />
       </View>
     </View>
   );
